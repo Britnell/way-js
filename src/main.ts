d66fb4604f3ae0b763b72ada42129d9a4db22d68
Britnell <britnell.h-d@partner.akdb.de>
2025-09-26 18:25:12 +0200

    web-comp

--- src/main.ts
+++ src/main.ts
@@ -83,6 +83,58 @@ class Framework {
       }
     });
   }
+
+  component(tag) {
+    const template = document.getElementById(tag);
+    if (template) {
+      createWebcomponent(tag, template);
+    }
+  }
+}
+
+class Component extends HTMLElement {
+  template: HTMLTemplateElement;
+  _data: any;
+
+  constructor(template: HTMLTemplateElement) {
+    super();
+    this.template = template;
+  }
+
+  connectedCallback() {
+    const content = this.template.content.cloneNode(true);
+    this.appendChild(content);
+
+    const counterData = { ...ff.comp.counter };
+
+    // Find parent x-data scope and evaluate props
+    const parentEl = this.closest('[x-data]');
+    if (parentEl && (parentEl as any)._data) {
+      const parentData = (parentEl as any)._data;
+      const propsAttr = this.getAttribute('x-props');
+      if (propsAttr) {
+        try {
+          const props = new Function('parent', `with(parent) { return ${propsAttr} }`)(parentData);
+          Object.assign(counterData, props);
+        } catch (e) {
+          console.error('Error evaluating props:', e);
+        }
+      }
+    }
+
+    this._data = (ff as any).createReactiveData(counterData);
+    (ff as any).bindDirectives(this, this._data);
+  }
+}
+
+function createWebcomponent(tag: string, template: HTMLElement) {
+  class WebComponent extends Component {
+    constructor() {
+      super(template);
+    }
+  }
+
+  customElements.define(tag, WebComponent);
 }
 
 const ff = new Framework();
@@ -99,7 +151,12 @@ ff.data('counter', {
   decrement() {
     this.x.value--;
   },
+  incr() {
+    this.x.value++;
+  },
 });
+
+ff.component('my-counter');
 
 document.addEventListener('DOMContentLoaded', () => {
   ff.hydrate(document.body);
