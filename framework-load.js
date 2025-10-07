4c0f0c273c830a496e7512915802eb4a8c1542a3
Britnell <britnell.h-d@partner.akdb.de>
2025-10-07 16:13:57 +0200

    rename to way.comp

--- framework-load.js
+++ framework-load.js
@@ -1,6 +1,6 @@
 // Sync loader - creates window.way.component that queues until way:init
 window.way = window.way || {};
-window.way.component = function(tag, setup) {
+window.way.comp = function (tag, setup) {
   document.addEventListener('way:init', () => {
     window.way.comp(tag, setup);
   });
